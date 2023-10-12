31b6bf4341a57a851982e83a60d9d76795e9de75
chacha <89715044+chachapp@users.noreply.github.com>
2023-10-12 21:58:58 +0900

    feat : 스웨거 API 상세 설명 추가

--- src/main/java/com/example/talktudy/controller/member/MemberController.java
+++ src/main/java/com/example/talktudy/controller/member/MemberController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 @Slf4j
 @RequestMapping("/api/member")
@@ -24,20 +25,20 @@ public class MemberController {
 
     @ApiOperation("회원 조회 api - 헤더의 토큰을 받아 회원 정보 조회")
     @GetMapping()
-    public ResponseEntity<MemberDTO> getMember(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+    public ResponseEntity<MemberDTO> getMember(@ApiIgnore @AuthenticationPrincipal CustomUserDetails customUserDetails) {
         return ResponseEntity.ok(memberService.getMember(customUserDetails.getMemberId()));
     }
 
     @ApiOperation("회원 수정 api - 헤더의 토큰과 정보를 받고, 회원 정보 수정.")
     @PutMapping()
-    public ResponseEntity<MemberDTO> updateMember(@AuthenticationPrincipal CustomUserDetails customUserDetails, MemberDTO memberDTO) {
+    public ResponseEntity<MemberDTO> updateMember(@ApiIgnore @AuthenticationPrincipal CustomUserDetails customUserDetails, MemberDTO memberDTO) {
         return ResponseEntity.ok(memberService.updateMember(customUserDetails.getMemberId(), memberDTO));
     }
 
     @ApiOperation("비밀번호 변경 api - 헤더의 토큰을 받고, 패스워드를 변경한다.")
     @ApiResponse(code = 400, message = "기존 비밀번호가 일치하지 않았을때, 비밀번호를 입력하지 않았을때 Bad Request로 리턴됩니다.")
     @PostMapping("/password")
-    public ResponseEntity<ResponseDTO> changePassword(@AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody PasswordRequest passwordRequest) {
+    public ResponseEntity<ResponseDTO> changePassword(@ApiIgnore @AuthenticationPrincipal CustomUserDetails customUserDetails, @RequestBody PasswordRequest passwordRequest) {
         return ResponseEntity.ok(memberService.changePassword(customUserDetails.getMemberId(), passwordRequest.getOldPassword(), passwordRequest.getNewPassword()));
     }
 
